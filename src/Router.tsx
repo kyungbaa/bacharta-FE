a9b3da5d224f51bbb6543f789c6ec36e277289b4
bbbb <bgh9651@gmail.com>
2022-09-25 13:54:54 +0900

    conflict 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,18 @@
-
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Maps from './pages/Map/Maps';
 import Main from './pages/Main/Main';
-import Login from "./components/Login/Login";
-import Outfits from "./pages/Outfits/Outfits";
+import Maps from './pages/Map/Maps';
+import Login from './components/Login/Login';
+import Outfits from './pages/Outfits/Outfits';
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/maps' element={<Maps />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/outfits" element={<Outfits />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/maps" element={<Maps />} />
+        <Route path="login" element={<Login />} />
+        <Route path="outfits" element={<Outfits />} />
       </Routes>
     </BrowserRouter>
   );
