e9e63d6c8e0e4875777c60366a776e4627228259
bbbb <bgh9651@gmail.com>
2022-10-04 10:59:10 +0900

    added weather mockdata

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -6,13 +6,14 @@ import clouds from '../../assets/weatherIcons/clouds.png';
 import rain from '../../assets/weatherIcons/rain.png';
 import snow from '../../assets/weatherIcons/snow.png';
 import sun from '../../assets/weatherIcons/sun.png';
+import { WEATHER_DATA } from '../../data/WEATHER_DATA';
 
 interface DataType {
   data: { title: string; lat: number; lng: number };
 }
 
 const Overlay = ({ data: { title, lat, lng } }: DataType) => {
-  const [temperature, setTemperature] = useState<Root>();
+  const [temperature, setTemperature] = useState<Root>(WEATHER_DATA);
   const [icon, setIcon] = useState<string>();
 
   interface Root {
@@ -70,58 +71,56 @@ const Overlay = ({ data: { title, lat, lng } }: DataType) => {
     sunset: number;
   }
 
-  const apiKey = process.env.REACT_APP_OPEN_WEATHER_KEY;
-
   useEffect(() => {
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${apiKey}`
-      )
-      .then((res) => {
-        let weatherIcons;
-        switch (res.data.weather[0].main) {
-          case 'Clear':
-            weatherIcons = sun;
-            break;
-          case 'Clouds':
-            weatherIcons = clouds;
-            break;
-          case 'Rain':
-            weatherIcons = rain;
-            break;
-          case 'Snow':
-            weatherIcons = snow;
-            break;
-          default:
-            weatherIcons = sun;
-        }
-        setTemperature(res.data);
-        setIcon(weatherIcons);
-      });
-
-    // api.getWeather(lat, lng).then((res) => console.log(res.data));
+    let weatherIcons;
+    switch (temperature.weather[0].main) {
+      case 'Clear':
+        weatherIcons = sun;
+        break;
+      case 'Clouds':
+        weatherIcons = clouds;
+        break;
+      case 'Rain':
+        weatherIcons = rain;
+        break;
+      case 'Snow':
+        weatherIcons = snow;
+        break;
+      default:
+        weatherIcons = sun;
+    }
+    setIcon(weatherIcons);
   }, []);
 
-  // const switchIcons = () => {
-  //   let weatherIcons = '';
-  //   switch (temperature.weather[0].main) {
-  //     case 'Clear':
-  //       weatherIcons = 'sun.png';
-  //       break;
-  //     case 'Clouds':
-  //       weatherIcons = 'cloudy.png';
-  //       break;
-  //     case 'Rain':
-  //       weatherIcons = 'rain.png';
-  //       break;
-  //     case 'Snow':
-  //       weatherIcons = 'cloudy.png';
-  //       break;
-  //     default:
-  //       weatherIcons = 'sun.png';
-  //   }
-  //   return weatherIcons;
-  // };
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${apiKey}`
+  //     )
+  //     .then((res) => {
+  //       let weatherIcons;
+  //       switch (res.data.weather[0].main) {
+  //         case 'Clear':
+  //           weatherIcons = sun;
+  //           break;
+  //         case 'Clouds':
+  //           weatherIcons = clouds;
+  //           break;
+  //         case 'Rain':
+  //           weatherIcons = rain;
+  //           break;
+  //         case 'Snow':
+  //           weatherIcons = snow;
+  //           break;
+  //         default:
+  //           weatherIcons = sun;
+  //       }
+  //       setTemperature(res.data);
+  //       setIcon(weatherIcons);
+  //     });
+
+  //   // api.getWeather(lat, lng).then((res) => console.log(res.data));
+  // }, []);
 
   return (
     <>
--- src/pages/Map/Maps.tsx
+++ src/pages/Map/Maps.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
 import { MARKER_DATA } from '../../data/MARKER_DATA';
 import Overlay from '../../components/Overlay/Overlay';
+import { useEffect } from 'react';
+import { getWeather } from '../../api/weatherAPI';
 
 const Maps = () => {
   const [level, setLevel] = useState(13);
