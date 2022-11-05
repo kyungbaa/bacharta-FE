537d4cf86b210d211afb82b6da4bbd19d8c9ce8d
bbbb <bgh9651@gmail.com>
2022-11-05 14:23:59 +0900

    added favicon 추가

--- src/pages/Map/Maps.tsx
+++ src/pages/Map/Maps.tsx
@@ -1,21 +1,19 @@
-import { useState } from "react";
-import { Map } from "react-kakao-maps-sdk";
-import { getWeather } from "../../api/weatherAPI";
-import WeatherOverlay from "./components/WeatherOverlay/WeatherOverlay";
-import Buttons from "./components/Buttons/Buttons";
-import MicrodustsOverlay from "./components/MicrodustsOverlay/MicrodustsOverlay";
-import { useQuery } from "@tanstack/react-query";
-import Loading from "../../components/Loading/Loading";
+import { useState } from 'react';
+import { Map } from 'react-kakao-maps-sdk';
+import { getWeather } from '../../api/weatherAPI';
+import WeatherOverlay from './components/WeatherOverlay/WeatherOverlay';
+import Buttons from './components/Buttons/Buttons';
+import MicrodustsOverlay from './components/MicrodustsOverlay/MicrodustsOverlay';
+import { useQuery } from '@tanstack/react-query';
+import Loading from '../../components/Loading/Loading';
 
 const Maps = () => {
   const [level] = useState(13);
   const [dataSwitch, setDataSwitch] = useState(true);
 
-  const { status, data } = useQuery(["weatherData"], () => getWeather(), {
-    retry: false,
-  });
+  const { status, data } = useQuery(['weatherData'], () => getWeather());
 
-  if (status === "loading") return <Loading />;
+  if (status === 'loading') return <Loading />;
 
   const weatherSwitch = () => {
     setDataSwitch(true);
@@ -27,9 +25,8 @@ const Maps = () => {
   return (
     <Map
       center={{ lat: 34.489127040010096, lng: 127.73040266182423 }}
-      style={{ width: "100%", height: "1000px", position: "relative" }}
-      level={level}
-    >
+      style={{ width: '100%', height: '1000px', position: 'relative' }}
+      level={level}>
       <Buttons
         weatherSwitch={weatherSwitch}
         microdustsSwitch={microdustsSwitch}
