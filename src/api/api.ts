a06b5e0d2caceb14212e468042c742e630269366
bbbb <bgh9651@gmail.com>
2022-11-23 18:01:25 +0900

    base url change

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const instance = axios.create({
-  baseURL: 'http://43.201.54.103:3001',
+  baseURL: BASE_URL,
 });
